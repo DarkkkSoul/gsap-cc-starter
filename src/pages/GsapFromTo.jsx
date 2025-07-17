ea568d413b12ecefaa46642c8d3011a6f7eea035
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-07-17 15:09:36 +0530

    gasp.fromTo() method

--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,42 +1,62 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFromTo = () => {
-  // TODO: Implement the gsap.fromTo() method
+    // TODO: Implement the gsap.fromTo() method
 
-  return (
-    <main>
-      <h1>GsapFromTo</h1>
+    useGSAP(() => {
+        gsap.fromTo('#red-box',
+            {
+                opacity: 0,
+                x: 50,
+                // repeat: -1,
+            },
+            {
+                delay: 0,
+                opacity: 1,
+                x: 0,
+                duration: 1,
+                repeat: -1,
+                yoyo: true,
+            })
+    }, []);
 
-      <p className="mt-5 text-gray-500">
-        The <code>gsap.fromTo()</code> method is used to animate elements from a
-        new state to a new state.
-      </p>
+    return (
+        <main>
+            <h1>GsapFromTo</h1>
 
-      <p className="mt-5 text-gray-500">
-        The <code>gsap.fromTo()</code> method is similar to the{" "}
-        <code>gsap.from()</code> and <code>gsap.to()</code> methods, but the
-        difference is that the <code>gsap.fromTo()</code> method animates
-        elements from a new state to a new state, while the{" "}
-        <code>gsap.from()</code> method animates elements from a new state to
-        their current state, and the <code>gsap.to()</code> method animates
-        elements from their current state to a new state.
-      </p>
+            <p className="mt-5 text-gray-500">
+                The <code>gsap.fromTo()</code> method is used to animate elements from a
+                new state to a new state.
+            </p>
 
-      <p className="mt-5 text-gray-500">
-        Read more about the{" "}
-        <a
-          href="https://greensock.com/docs/v3/GSAP/gsap.fromTo()"
-          target="_blank"
-          rel="noreferrer noopener nofollow"
-        >
-          gsap.fromTo()
-        </a>{" "}
-        method.
-      </p>
+            <p className="mt-5 text-gray-500">
+                The <code>gsap.fromTo()</code> method is similar to the{" "}
+                <code>gsap.from()</code> and <code>gsap.to()</code> methods, but the
+                difference is that the <code>gsap.fromTo()</code> method animates
+                elements from a new state to a new state, while the{" "}
+                <code>gsap.from()</code> method animates elements from a new state to
+                their current state, and the <code>gsap.to()</code> method animates
+                elements from their current state to a new state.
+            </p>
 
-      <div className="mt-20">
-        <div id="red-box" className="w-20 h-20 bg-red-500 rounded-lg" />
-      </div>
-    </main>
-  );
+            <p className="mt-5 text-gray-500">
+                Read more about the{" "}
+                <a
+                    href="https://greensock.com/docs/v3/GSAP/gsap.fromTo()"
+                    target="_blank"
+                    rel="noreferrer noopener nofollow"
+                >
+                    gsap.fromTo()
+                </a>{" "}
+                method.
+            </p>
+
+            <div className="mt-20">
+                <div id="red-box" className="w-20 h-20 bg-red-500 rounded-lg" />
+            </div>
+        </main>
+    );
 };
 
 export default GsapFromTo;
